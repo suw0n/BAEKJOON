709b5a8e1a9693c5bb543d52ecc3ab9022b3d126
최수원 <suwon3274@gmail.com>
2022-10-22 12:40:19 +0900

    10.22

---
+++ 2100/2145/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n, re;
+        while(true){
+            n = Integer.parseInt(br.readLine());
+            re = n;
+            if(n==0)
+                break;
+            while(re > 9){
+                re = 0;
+                while(n != 0){
+                    re += n%10;
+                    n/=10;
+                }
+                n = re;
+            }
+            System.out.println(re);
+        }
+    }
+
+}
