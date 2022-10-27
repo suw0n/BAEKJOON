0b749c042d2b8661df46a33863f30302d1769f1e
최수원 <suwon3274@gmail.com>
2022-10-27 10:24:30 +0900

    10.27

---
+++ 23500/23530/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        while(t-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            System.out.println(1);
+        }
+    }
+
+}
