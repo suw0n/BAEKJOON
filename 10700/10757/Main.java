09eb1dcebe5155d0836c7071633f28f772d8d794
최수원 <suwon3274@gmail.com>
2022-11-06 20:00:52 +0900

    11.6

---
+++ 10700/10757/Main.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+        System.out.print(a.add(b));
+    }
+
+}
