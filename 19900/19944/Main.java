87f45f39c1a1b854653cb453445e1dcc20a33fdc
최수원 <suwon3274@gmail.com>
2022-12-11 10:17:24 +0900

    12.11

---
+++ 19900/19944/Main.java
@@ -0,0 +1,15 @@
+import java.util.StringTokenizer;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        System.out.print(m==1||m==2?"NEWBIE!":n>=m?"OLDBIE!":"TLE!");
+    }
+
+}
