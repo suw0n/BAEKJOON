976780e06f2e3f57f94792f7b114e16ac8b3a9f0
최수원 <suwon3274@gmail.com>
2022-12-31 12:12:26 +0900

    12.31

--- 14400/14495/Main.java
+++ 14400/14495/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
         long[] dp = new long[n+5];
         dp[1] = dp[2] = dp[3] = 1;
