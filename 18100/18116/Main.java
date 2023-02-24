65906b583d19ba8dbfcc2077a589a424993452d5
최수원 <suwon3274@gmail.com>
2023-02-24 18:10:01 +0900

    fix

--- 18100/18116/Main.java
+++ 18100/18116/Main.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
